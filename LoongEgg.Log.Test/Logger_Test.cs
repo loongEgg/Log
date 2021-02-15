8b7d48c1ef7e7dfe10ee7c4a31cd9e69d4b8d644
loongEgg <loongEgg@users.noreply.github.com>
2021-02-15 16:32:51 +0800

    第一次完整功能提交

---
+++ LoongEgg.Log.Test/Logger_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace LoongEgg.Log.Test
+{
+    [TestClass]
+    public class Logger_Test
+    {
+        [TestMethod]
+        public void EachLogger_IsSingletonInDictionary()
+        {
+            Logger.Clear();
+
+            Logger.Enable(Loggers.Console ^ Loggers.Debug ^ Loggers.File);
+            Logger.Enable(Loggers.Console ^ Loggers.Debug);
+
+            Assert.IsTrue(Logger.LoggerDict.Count == 3);
+        }
+
+        [TestMethod]
+        public void FileLogger_CanCreateFile()
+        {
+            Logger.Clear();
+            Logger.Enable(Loggers.File);
+
+            Assert.IsTrue(File.Exists((Logger.LoggerDict[Loggers.File] as FileLogger).FileName));
+
+        }
+
+        [TestMethod]
+        public void FileLogger_CanWriteLog()
+        {
+
+            Logger.Clear();
+            Logger.Enable(Loggers.File);
+            Logger.Dbug("a debug message", true);
+            Logger.Info("a info message", true);
+            Logger.Warn("a warn message", true);
+            Logger.Erro("a erro message", true);
+
+            string fileName = (Logger.LoggerDict[Loggers.File] as FileLogger).FileName;
+            Assert.IsTrue(File.Exists(fileName));
+
+            using (StreamReader fs = new StreamReader(fileName))
+            {
+                var log = fs.ReadToEnd();
+                Assert.IsTrue(log.Contains("debug"));
+                Assert.IsTrue(log.Contains("info"));
+                Assert.IsTrue(log.Contains("warn"));
+                Assert.IsTrue(log.Contains("erro"));
+            }
+        }
+
+    }
+}
---
+++ LoongEgg.Log.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LoongEgg.Log.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LoongEgg.Log.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b8f462ea-dff9-4262-ac60-9bd0f10d76f9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
