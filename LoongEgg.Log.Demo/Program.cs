7a70ab8973232edfb3604ca50e628ec62e194469
loongEgg <loongEgg@users.noreply.github.com>
2021-02-15 17:23:10 +0800

    release 0.1.0

---
+++ LoongEgg.Log.Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoongEgg.Log.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Enable(Loggers.Console | Loggers.Debug | Loggers.File);
+
+            Logger.Dbug("debug");
+            Logger.Info("information");
+            Logger.Warn("warning");
+            Logger.Erro("error");
+
+            Logger.Clear();
+
+            Console.ReadKey();
+        }
+    }
+}
